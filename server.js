d936047b87c82f64f7051209b9ef401da685ee6b
Seth Jernigan <SethJernigan8@outlook.com>
2023-11-16 22:20:56 -0700

    Initial server side login/add user implementation

--- server.js
+++ server.js
@@ -1,1 +1,120 @@
-//start
+const mongoose = require('mongoose');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const crypto = require('crypto');
+const port = 80;
+
+const db = mongoose.connection;
+const mongoDBURL = 'mongodb://127.0.0.1/337chat';
+mongoose.connect(mongoDBURL, {useNewUrlParser: true});
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+    username: String,
+    password: String, // temporary
+    hash: Number,
+    salt: Number
+});
+var User = mongoose.model('User', UserSchema);
+
+let sessions = {};
+
+function addSession(username) {
+
+    let sid = Math.floor(Math.random() * 1000000000);
+    let now = Date.now();
+    sessions[username] = {id: sid, time: now};
+    return sid;
+}
+
+function removeSessions() {
+
+    let now = Date.now();
+    let usernames = Object.keys(sessions);
+    for (let i = 0; i < usernames.length; i++) {
+        let last = sessions[usernames[i]].time;
+        if (last + 60000 < now) { // TODO - change - currently 1 minute
+            delete sessions[usernames[i]];
+            console.log("" + usernames[i] + " session deleted");
+        }
+    }
+}
+
+setInterval(removeSessions, 2000);
+
+const app = express()
+app.use(express.json());
+app.use(cookieParser());
+
+function authenticate(req, res, next) {
+
+    let c = req.cookies;
+    console.log('auth request:');
+    console.log(req.cookies);
+    if (c != undefined) {
+        if (sessions[c.login.username] != undefined && 
+        sessions[c.login.username].id == c.login.sessionID) {
+            next();
+        } else {
+            res.redirect('./public_html/index.html');
+        }
+    }else {
+        res.redirect('./public_html/index.html');
+    }
+}
+
+app.use('/app/*', authenticate);
+app.get('/app/*', (req, res, next) => {
+    console.log('another');
+    next();
+});
+
+app.use(express.static('public_html'));
+
+app.post('/account/login/', function(req, res) {
+    let usernameIn = req.body.username;
+    let passwordIn = req.body.password;
+
+    let p = User.find({'username':usernameIn, 'password':passwordIn}).exec();
+    p.then((documents) => {
+        if (documents.length == 0){
+            res.end('Login Failed');
+        } else {
+            let sid = addSession(usernameIn);
+            res.cookie("login", 
+                {username: usernameIn, sessionID: sid},
+                {maxAge: 60000 * 2}); // create a new cookie with 2 minutes life
+            res.end('SUCCESS');
+        }
+    });
+    console.log('Logged in');
+});
+
+app.post('/add/user/', function(req, res) {
+    /* 
+        Adds a new user to the User collection of the database.
+    */
+    let usernameIn = req.body.username;
+    let passwordIn = req.body.password;
+    let listingsIn = [];
+    let purchasesIn = [];
+
+    let p1 = User.find({'username': usernameIn}).exec();
+    p1.then( (results) => {
+        if (results.length == 0){
+            let newUser = new User({username: usernameIn, password: passwordIn, listings: listingsIn, purchases: purchasesIn});
+            let p = newUser.save();
+            p.then(() => {
+                res.end('USER CREATED!');
+            });
+            p.catch(() => {
+                res.end('DATABASE SAVE ISSUE');
+            });
+        } else {
+            res.end('USERNAME ALREADY TAKEN')
+        }
+    })
+});
+
